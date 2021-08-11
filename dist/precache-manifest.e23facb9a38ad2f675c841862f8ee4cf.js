51fb55e9785c010c994f82b790042f369e738b1e
Sleepyctrl <123qwe4r@ro.ru>
2021-08-11 18:17:54 +0300

    all files project(big fail)

---
+++ dist/precache-manifest.e23facb9a38ad2f675c841862f8ee4cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f027e68a66975a1dbe9e",
+    "url": "/css/chunk-vendors.07ce6cfd.css"
+  },
+  {
+    "revision": "ae54d912d011300ff856d2013f3b593a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef5640560ebe6ef34c19",
+    "url": "/js/about.da1d0ad9.js"
+  },
+  {
+    "revision": "d4e145dabd4021e9d550",
+    "url": "/js/app.1d341a3d.js"
+  },
+  {
+    "revision": "f027e68a66975a1dbe9e",
+    "url": "/js/chunk-vendors.8357bec4.js"
+  },
+  {
+    "revision": "0c123a881fde91d73d2b9b418829ac92",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
